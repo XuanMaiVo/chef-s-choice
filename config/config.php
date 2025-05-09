a90930fbd5b0a3d1d30f7d4be3af593a26e90fc6
TrucDeThuong <hvthanhtruc121203@gmail.com>
2025-05-09 22:16:44 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,9 +1,20 @@
 <?php
+<<<<<<< Updated upstream
 $host = "localhost";
 $username = "root";
 $password = "";
 $database = "chef_db"; 
 $conn = mysqli_connect($host, $username, $password, $database);
+=======
+define("DB_HOST", "localhost:3307"); //tại tui xài port này nên tui dể 3307
+define("DB_USER", "root");
+define("DB_PASS", ""); //nếu có mkhau thig mng set mkhau lại nha
+define("DB_NAME", "chef_db");
+?>
+<!--
+db_user với db_pass là để theo cái xampp hoặc mysql
+của mng á nha
+>>>>>>> Stashed changes
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
